a0355644799a88a41be1a0df2fce9958c97bc61f
jooy3410 <jooy3410@naver.com>
2022-03-11 09:18:54 +0900

    first

---
+++ 2438.py
@@ -0,0 +1,8 @@
+a = input()
+a = int(a)
+
+for i in range(1, a+1):
+    print('*' * i)
+
+
+    
---
+++ 9498.py
@@ -0,0 +1,17 @@
+t = input()
+t = int(t)
+
+if t <= 100 and t >= 90 :
+    print('A')
+
+elif t <= 89 and t >= 89 :
+    print('B')
+
+elif t <= 79 and t >= 70:
+    print('C')
+
+elif t <= 69 and t >= 60:
+    print('D')
+
+else:
+    print('F')
